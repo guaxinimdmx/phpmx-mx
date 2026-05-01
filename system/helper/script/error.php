82189fa819a57766f5a33d101f605ff004b11812
Anré Ricardo <contatoadmx@gmail.com>
2026-05-01 00:43:09 -0300

    no deprecated error

--- system/helper/script/error.php
+++ system/helper/script/error.php
@@ -3,7 +3,7 @@
 if (env('DEV')) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 } else {
     ini_set('display_errors', 0);
     ini_set('display_startup_errors', 0);
